bf6a177d189b147852d1581cb4ba827b1864b457
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-07 17:11:49 -0700

    Test for values of #consistent_hash.

--- spec/consistent_hash_spec.rb
+++ spec/consistent_hash_spec.rb
@@ -4,4 +4,15 @@ describe String do
   it 'creates a String#consistent_hash method' do
     expect("").to respond_to(:consistent_hash)
   end
+
+  it 'works just like Java String#hashCode' do
+    examples = {
+      "" => 0,
+      "hello world" => 1794106052,
+      "this is a test" => -1879005787,
+      "a" * 200 => 469437568
+    }
+
+    examples.each { |k, v| expect(k.consistent_hash).to eq v }
+  end
 end
