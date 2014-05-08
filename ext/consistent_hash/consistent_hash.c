dbba0e351aaafab7e74aba915771432055bf76f0
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-07 17:12:27 -0700

    Add a C implementation of #consistent_hash.

---
+++ ext/consistent_hash/consistent_hash.c
@@ -0,0 +1,16 @@
+#include "ruby.h"
+
+VALUE consistent_hash(VALUE string) {
+  uint32_t h = 0;
+
+  for (int i = 0; i < RSTRING_LEN(string); i++) {
+    uint32_t c = RSTRING_PTR(string)[i];
+    h = h * 31 + c;
+  }
+
+  return INT2NUM(h);
+}
+
+void Init_consistent_hash() {
+  rb_define_method(rb_cString, "consistent_hash", consistent_hash, 0);
+}
